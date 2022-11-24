62a5f9d398189467da310cd8b483535c640c78e4
badr ghanbi <bghanbi50@gmail.com>
2022-11-24 19:02:30 +0100

    inital typography styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 
 function App() {
   return (
-   <h1>hello world</h1>
+  <>
+  <h1>More than just shorter links</h1>
+  <p>
+  Build your brand's recognition and get detailed insights on how your links are performing.
+  </p>
+  <h2>
+  Advanced Statistics
+  </h2>
+  <h3>
+  Brand Recognition
+  </h3>
+
+  </>
   );
 }
 
