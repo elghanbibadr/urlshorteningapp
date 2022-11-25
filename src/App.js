dbd0999af023a40d7fff8abfc8b54ce123672b18
badr ghanbi <bghanbi50@gmail.com>
2022-11-25 14:16:21 +0100

    add styles for buttons

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@
 function App() {
   return (
   <>
-  <h1>More than just shorter links</h1>
-  <p>
-  Build your brand's recognition and get detailed insights on how your links are performing.
-  </p>
-  <h2>
-  Advanced Statistics
-  </h2>
-  <h3>
-  Brand Recognition
-  </h3>
+  <button className="btn btn--rounded">Get started</button>
 
   </>
   );
