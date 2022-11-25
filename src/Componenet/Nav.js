67fb597fc5b76fd754c8f0d73da380d2989eff84
badr ghanbi <bghanbi50@gmail.com>
2022-11-25 15:34:34 +0100

    create the nav html

--- src/Componenet/Nav.js
+++ src/Componenet/Nav.js
@@ -1,6 +1,22 @@
+
 const Nav=()=>{
     return(
-        <h1>yoo</h1>
+        <header>
+            <nav className="nav">
+             <img className="nav__logo" src='/images/logo.svg'/>
+             <img className="nav__hamburger" src='/images/bars-solid.svg'/>
+             <img className="nav__close" src='/images/xmark-solid.svg'/>
+             <ul className="nav__list hidden">
+                <li className="nav__list-link">Features</li>
+                <li className="nav__list-link">Pricing</li>
+                <li className="nav__list-link">Resources</li>
+             </ul>
+             <div className="nav__btn-box hidden">
+                <button className="nav__btn-login">Login</button>
+                <button className="nav__btn-signUp btn btn--rounded">Sign up</button>
+             </div>
+            </nav>
+        </header>
     )
 }
 
