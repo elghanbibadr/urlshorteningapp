40a14b104e6532726ef499715a8a691e12604afe
badr ghanbi <bghanbi50@gmail.com>
2022-11-28 12:35:03 +0100

    fix the damn hero section bugg fuck you svg

--- src/Componenet/Hero.js
+++ src/Componenet/Hero.js
@@ -1,10 +1,14 @@
 const Hero=()=>{
     return(
-        <section className="hero">
+        <section className="hero grid grid grid--cls2">
          <div className="hero__image-container">
-            <img src='/images/illustration-working.svg' className="hero__image"/>
+            <img src="/images/illustration-working.svg" className="hero__img"/>
          </div>
-         <h1>hello</h1>
+         <div className="hero__content">
+            <h1 className="hero__title">More than just shorter links</h1>
+            <p className="hero__desc">Build your brand's recognition and get detailed insights on how your links are performing.</p>
+            <button className="hero__btn btn btn--rounded" >Get Started</button>
+         </div>
         </section>
     )
 }
