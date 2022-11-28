80170a8783f80259b1ea099080c38fe5f5b65cb6
badr ghanbi <bghanbi50@gmail.com>
2022-11-28 12:54:03 +0100

    create the url shortening form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Nav from "./Componenet/Nav";
 import './styles/main.css';
 import Hero from "./Componenet/Hero";
+import Hero from "./Componenet/UrlShortenForm";
+import UrlShortenForm from "./Componenet/UrlShortenForm";
 
 function App() {
   return (
   <>
  <Nav/>
   <Hero/>
+  <UrlShortenForm/>
 </>
   );
 }
---
+++ src/Componenet/UrlShortenForm.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const  UrlShortenForm=(props)=> {
+    return (
+        <div className='container'>
+            <h1>hello</h1>
+        </div>
+    );
+}
+
+export default UrlShortenForm;
+// https://api.shrtco.de/v2/shorten?url=${link}`
