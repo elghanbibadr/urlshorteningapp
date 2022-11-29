7b963bf55d659955897ebaf45715d92096d517fe
badr ghanbi <bghanbi50@gmail.com>
2022-11-29 11:25:37 +0100

    make api request with axios

--- src/Componenet/UrlShortenForm.js
+++ src/Componenet/UrlShortenForm.js
@@ -1,11 +1,29 @@
 
-import React from 'react';
+import React ,{useState,useEffect} from 'react';
+import axios from 'axios';
 
 
-const  UrlShortenForm=(props)=> {
+const  UrlShortenForm= (props)=> {
+    const url='https://levelup.gitconnected.com/fetch-api-data-with-axios-and-display-it-in-a-react-app-with-hooks-3f9c8fa89e7b';
+    const [enteredUrl,setEnteredUrl]=useState('');
+    const [shortened,setShortened]=useState([]);
+    const handleFormSubmition=(event)=>{
+        event.preventDefault();
+        let inputValue=event.target.firstElementChild.value;
+        if (!inputValue)return;
+        setEnteredUrl(inputValue);
+        axios.get(`https://api.shrtco.de/v2/shorten?url=${inputValue}`
+        )
+        .then(res=>{
+           setShortened(res.data.result);
+           console.log(shortened)
+
+        }).catch(err=>alert(err))
+    }
+
     return (
         <div className='container'>
-           <form className='urlShortenForm'>
+           <form onSubmit={handleFormSubmition} className='urlShortenForm'>
             <input className='urlShortenForm__input' type="text" placeholder='Shorten a link here'/>
             <button className='btn urlShortenForm__btn'>Shorten it!</button>
            </form>
