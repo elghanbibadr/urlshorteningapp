6ccaee83247deb2b8ec0372ce40bc3c5893d57df
badr ghanbi <bghanbi50@gmail.com>
2022-11-30 11:22:49 +0100

    add src image to each card

--- src/Componenet/Statistics.js
+++ src/Componenet/Statistics.js
@@ -8,8 +8,8 @@ function Statistics() {
              <p className='statistics__desc'>Track how your links are performing across the web with our advanced statistics dashboard.</p>
              <div className='statistics__card-container'>
                <StatisticCard src='/images/icon-brand-recognition.svg' title='Brand Recognition' text='Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.'/>
-               <StatisticCard src='/images/icon-brand-recognition.svg' title='Detailed Records' text='Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'/>
-               <StatisticCard src='/images/icon-brand-recognition.svg' title='Fully Customizable' text='Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'/>        
+               <StatisticCard src='/images/icon-detailed-records.svg' title='Detailed Records' text='Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.'/>
+               <StatisticCard src='/images/icon-fully-customizable.svg' title='Fully Customizable' text='Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.'/>        
              </div>
         </section>
     );
